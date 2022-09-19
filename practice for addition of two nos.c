12d01a38afd8fb6ec5d22be9059a936fdd0b1547
tigerak1234 <4500.stkabirnav@gmail.com>
2022-09-19 14:07:58 +0530

    Create practice for addition of two nos.c

---
+++ practice for addition of two nos.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int num1,num2;
+	printf("Enter two numbers:\n");
+	printf("Enter number 1 = ",num1);
+	scanf("%i",&num1);
+	printf("Enter number 2 = ",num2);
+	scanf("%i",&num2);
+	printf("\nTotal of both numbers is : %i",num1+num2);
+	printf("\nSubtraction of both numbers is : %i",num1-num2);
+	printf("\nMultiplication of both numbers is : %i",num1*num2);
+	printf("\nDivision of both numbers is : %i",num1/num2);
+	printf("\nMod of both numbers is : %i",num1%num2);
+	
+	
+	
+	
+	
+	
+	return 0;
+}
