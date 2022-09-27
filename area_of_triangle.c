8fb86e68d01a34ec7330ea13355bf9e6edf3dc5d
tigerak1234 <4500.stkabirnav@gmail.com>
2022-09-27 20:29:43 +0530

    Create area_of_triangle.c

---
+++ area_of_triangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	float base,height;
+	printf("Enter base dimension = ");
+	scanf("%f",&base);
+	printf("Enter height dimension = ");
+	scanf("%f",&height);
+	printf("Area of triangle is %.2f ",0.5*base*height);
+	
+	
+	
+	return 0;
+}
