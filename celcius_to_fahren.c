a73b2364c8e9a018c651f4d6fbd474cca715ad12
tigerak1234 <4500.stkabirnav@gmail.com>
2022-09-27 20:29:39 +0530

    Create celcius_to_fahren.c

---
+++ celcius_to_fahren.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	float celcius;
+	printf("Enter celcius = ");
+	scanf("%f",&celcius);
+	printf("The conversion of %.f to fahrenheit is %.2f",celcius,celcius*1.8+32);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
