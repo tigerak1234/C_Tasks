efd594ec35b95a5cb564090ee14d7516ab411cd4
tigerak1234 <4500.stkabirnav@gmail.com>
2022-09-27 18:53:51 +0530

    Create square_cube.c

---
+++ square_cube.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int num1;
+	printf("Enter a number = ");
+	scanf("%i",&num1);
+	printf("The square of %i is %i",num1,num1*num1);
+	printf("\nThe cube of %i is %i",num1,num1*num1*num1);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
