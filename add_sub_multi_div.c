44c4b973ccfa359d4611208ef63e5460e8e1b6ca
tigerak1234 <4500.stkabirnav@gmail.com>
2022-09-27 18:44:41 +0530

    Create add_sub_multi_div.c

---
+++ add_sub_multi_div.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+ 	int num1,num2;
+ 	printf("Enter number 1 = ");
+ 	scanf("%i",&num1);
+ 	printf("\nEnter number 2 = ");
+ 	scanf("%i",&num2);
+ 	
+ 	printf("\nAddition of %i and %i is %i",num1,num2,num1+num2);
+ 	printf("\nSubtraction of %i and %i is %i",num1,num2,num1-num2);
+ 	printf("\nMultiplication of %i and %i is %i",num1,num2,num1*num2);
+ 	printf("\nDivision of %i and %i is %i",num1,num2,num1/num2);
+ 	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
