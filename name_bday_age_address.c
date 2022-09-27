827d8d6d856e1eafb3abb6f92a3691c47005f169
tigerak1234 <4500.stkabirnav@gmail.com>
2022-09-27 18:36:28 +0530

    Create name_bday_age_address.c

---
+++ name_bday_age_address.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	printf("Name = Walter Hartwell White");
+	printf("\nBirthday = September 7 1958");
+	printf("\nAge = 52");
+	printf("\nAddress = 308 Negra Arroyo Lane, Albuquerque, New Mexico");
+	
+	
+	
+	
+	
+	
+	return 0;
+}
