bbb970121768b1f5bac325503b742f4f93071c7c
tigerak1234 <4500.stkabirnav@gmail.com>
2022-09-27 11:41:17 +0530

    Create vowel_or_consonant.c

---
+++ vowel_or_consonant.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+	
+	char a;
+	printf("Enter an alphabet : ");
+	scanf("%c",&a);
+	if(a == 'a' || a == 'A' || a == 'e' || a == 'E' || a == 'i' || a == 'I' || a == 'o' || a == 'O' || a == 'u' || a == 'U')
+	{
+		printf("The alphabet is a vowel");
+}
+	else
+	{
+		printf("The alphabet is a consonant");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
