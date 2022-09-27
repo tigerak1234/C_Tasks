5c29edd098ed44cf8cdc2203bb67ebeefdb33155
tigerak1234 <4500.stkabirnav@gmail.com>
2022-09-27 13:32:59 +0530

    Update divisible_by_5_and_11.c

--- divisible_by_5_and_11.c
+++ divisible_by_5_and_11.c
@@ -8,11 +8,11 @@ int main()
 	
 	if(a%5 == 0 && a%11 == 0)
 	{
-		printf("The number is divisible by 5 and 11");
+		printf("%i is divisible by 5 and 11",a);
 	}
 	else
 	{
-		printf("The number is not divisible by 5 and 11");
+		printf("%i is not divisible by 5 and 11",a);
 	}
 	
 	
