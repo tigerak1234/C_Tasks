4701f5dadea94b04a9879df5099e76394d1cfd5c
tigerak1234 <4500.stkabirnav@gmail.com>
2022-09-27 20:29:26 +0530

    Create area_of_circle.c

---
+++ area_of_circle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	
+	float radius;
+	printf("Enter radius = ");
+	scanf("%f",&radius);
+	printf("The area of the circle is %.2f",3.14*radius*radius);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
