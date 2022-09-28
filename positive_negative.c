8cff0a94b88f0f9a75eb2cd49a03333083090115
tigerak1234 <4500.stkabirnav@gmail.com>
2022-09-28 22:50:46 +0530

    Create positive_negative.c

---
+++ positive_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int num1;
+	printf("Enter an integer : ");
+	scanf("%i",&num1);
+	if(num1>=0)
+	{
+		printf("The number is positive.");
+	}
+	else
+	{
+		printf("The number is negative.");
+	}
+
+	
+	
+	
+	
+	
+	
+	return 0;
+}
